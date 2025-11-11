11d93099c6761169e52fea861ecd0b5248c6d6ff
Vinícius Stencel Canto Silva <stencel.vinicius@gmail.com>
2025-11-11 01:31:40 -0300

    ✏️ refactor(ResidueNotificationService): Removendo comentários desnecessários

--- services/persistence-java/src/main/java/br/com/unicuritiba/ecosort/application/services/ResidueNotificationService.java
+++ services/persistence-java/src/main/java/br/com/unicuritiba/ecosort/application/services/ResidueNotificationService.java
@@ -10,7 +10,6 @@ import org.springframework.stereotype.Service;
 @Service
 public class ResidueNotificationService {
 
-    // Adicionar estas duas linhas:
     private static final Logger logger = LoggerFactory.getLogger(ResidueNotificationService.class);
 
     public static final String DESTINATION_TOPIC = "/topic/residues";
@@ -24,13 +23,13 @@ public class ResidueNotificationService {
 
     public void sendResidueUpdate(ResidueDTO residue) {
         try {
-            logger.info("Tentando enviar resíduo para o tópico: {}", DESTINATION_TOPIC); // <-- Adicionar
+            logger.info("Tentando enviar resíduo para o tópico: {}", DESTINATION_TOPIC);
             messagingTemplate.convertAndSend(DESTINATION_TOPIC, residue);
-            logger.info("Resíduo enviado com sucesso: {}", residue.classification()); // <-- Adicionar
+            logger.info("Resíduo enviado com sucesso: {}", residue.classification());
 
         } catch (Exception e) {
-            // Se algo der errado, veremos isso no log do Spring Boot
-            logger.error("!!! FALHA AO ENVIAR MENSAGEM WEBSOCKET !!!", e); // <-- Adicionar
+
+            logger.error("!!! FALHA AO ENVIAR MENSAGEM WEBSOCKET !!!", e);
         }
     }
 }
