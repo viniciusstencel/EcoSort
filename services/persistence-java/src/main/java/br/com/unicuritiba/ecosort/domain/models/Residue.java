6c2c97bf40e4ddc669f881e381bc7af25bf9db54
Vinícius Stencel Canto Silva <stencel.vinicius@gmail.com>
2025-11-11 01:33:47 -0300

    ♻️ refactor(Residue): Adicionando anotações necessárias

--- services/persistence-java/src/main/java/br/com/unicuritiba/ecosort/domain/models/Residue.java
+++ services/persistence-java/src/main/java/br/com/unicuritiba/ecosort/domain/models/Residue.java
@@ -1,8 +1,6 @@
 package br.com.unicuritiba.ecosort.domain.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import java.time.LocalDateTime;
@@ -15,6 +13,7 @@ import java.util.UUID;
 public class Residue {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
     private String classification;
